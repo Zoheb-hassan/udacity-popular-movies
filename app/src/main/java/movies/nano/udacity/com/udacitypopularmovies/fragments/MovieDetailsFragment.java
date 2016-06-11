4f4d45d7a04cd0832b6f953afb99832ccde41874
Zoheb Syed <zoheb.syed@outlook.com>
2016-06-11 19:31:02 +0530

    Multi Pane Layout First Steps

---
+++ app/src/main/java/movies/nano/udacity/com/udacitypopularmovies/fragments/MovieDetailsFragment.java
@@ -0,0 +1,33 @@
+package movies.nano.udacity.com.udacitypopularmovies.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ZohebS on 11-06-2016.
+ */
+public class MovieDetailsFragment extends Fragment {
+
+
+    public MovieDetailsFragment getInstance(int position){
+
+        return null;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+
+}
---
+++ app/src/main/java/movies/nano/udacity/com/udacitypopularmovies/fragments/PositionChangeListener.java
@@ -0,0 +1,10 @@
+package movies.nano.udacity.com.udacitypopularmovies.fragments;
+
+/**
+ * Created by ZohebS on 11-06-2016.
+ */
+public interface PositionChangeListener {
+
+    public void onPositionChangeListener(int positionSelected);
+
+}
